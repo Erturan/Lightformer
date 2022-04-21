416ff42d241ef1c46da22986c1a6ec603bd04a71
Erturan <47361021+Erturan@users.noreply.github.com>
2022-04-21 16:20:54 +0100

    Level and health displayed

---
+++ src/Level.java
@@ -0,0 +1,8 @@
+public class Level {
+
+    private void generateLeve(int levelNo) {
+        if (levelNo == 1) {
+
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 import processing.core.PApplet;
+import processing.core.PImage;
 import processing.core.PVector;
+
+import java.util.ArrayList;
 
 public class Main extends PApplet {
 
@@ -10,6 +13,12 @@ public class Main extends PApplet {
     static final int MY_WIDTH = 1920;
     static final int MY_HEIGHT = 1080;
 
+    static int level = 1;
+
+    static PImage imgHeart;
+
+    ArrayList<Powerup> powerups = new ArrayList<>();
+
     static final PVector gravity = new PVector(0f, 0.04f);
     Player player;
     public void settings() {
@@ -18,12 +27,25 @@ public class Main extends PApplet {
 
     public void setup() {
         player = new Player(this);
+        imgHeart = loadImage("heart.png");
+        imgHeart.resize(50,50);
     }
 
     public void draw() {
         background(128);
         player.integrate();
         circle(player.pos.x, player.pos.y, player.getLightRadius());
+
+        pushStyle();
+        for (int i = 0; i < player.health; i++) {
+            image(imgHeart, 50 + i * 60, 1000);
+        }
+
+        pushStyle();
+        textSize(50);
+        textAlign(CENTER, TOP);
+        text("Level " + level, displayWidth / 2, 1000);
+        popStyle();
 
     }
 
